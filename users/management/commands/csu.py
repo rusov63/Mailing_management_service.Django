11359ada1e936c4670bff10a809227c285a7b0a7
rusov63 <rusov63@yahoo.com>
2023-07-26 09:36:57 +0400

    исправление

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -4,18 +4,18 @@ from dotenv import load_dotenv
 import os
 
 
-PASSWORD = os.getenv('Pass_SQL') # введите пароль
-EMAIL_USER = os.getenv('@'), # укажите почту для входа в админ панель
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD') # введите пароль
+EMAIL_USER = os.getenv('EMAIL_USER'), # укажите почту для входа в админ панель
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email= EMAIL_USER,
+            email=EMAIL_USER,
             first_name='Admin',
             last_name='SuperUser',
             is_staff=True,
             is_superuser=True
         )
 
-        user.set_password(PASSWORD)
+        user.set_password(EMAIL_PASSWORD)
         user.save()
